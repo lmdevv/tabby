e304b22ec1a2430067adbe196381eb6bab5432c9
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-09-17 17:21:39 -0400

    refactor core data types
    - update tab, workspace, and resource interfaces
    - add resource group interface

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,9 @@
 import type { Browser } from "wxt/browser";
 
 export interface Tab extends Browser.tabs.Tab {
-  workspaceId?: number;
+  stableId: string;
+  workspaceId: number;
+  tabStatus: "active" | "archived";
   tags?: string[];
   description?: string;
   createdAt: number;
@@ -9,26 +11,41 @@ export interface Tab extends Browser.tabs.Tab {
 }
 
 export interface Workspace {
-  id?: number;
-  groupId: number;
+  id: number;
+  groupId?: number;
   name: string;
   description?: string;
   createdAt: number; // epoch ms
   lastOpened: number;
-  opened: number; // 1 true 0 false
+  active: 1 | 0;
+  resourceGroupIds: number[]; // Ordered list of resource group IDs in this workspace
 }
 
 export interface WorkspaceGroup {
   id: number;
   name: string;
   icon?: string;
-  workspaces: Workspace[];
+  collapsed: 1 | 0; // true or false
 }
 
-export interface Resource {
-  id?: number;
-  name: string;
+export type Resource = Pick<
+  Browser.tabs.Tab,
+  "url" | "title" | "favIconUrl"
+> & {
+  id: number;
+  tags?: string[];
   description?: string;
   createdAt: number;
-  lastAccessed: number;
+  updatedAt: number;
+};
+
+export interface ResourceGroup {
+  id: number;
+  name: string;
+  collapsed: 1 | 0;
+  resourceIds: string[]; // Ordered list of resource IDs in this group
+  description?: string;
+  createdAt: number;
+  updatedAt: number;
+}
 }
