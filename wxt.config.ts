af24c6d7f436377a5bc66a345fecf0adaebc9c38
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-09-17 11:59:40 -0400

    update manifest permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    permissions: ["tabs", "storage", "tabGroups"],
+  },
   vite: () => ({
     plugins: [tailwindcss()],
     resolve: {
