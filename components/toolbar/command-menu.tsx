e435b06632b0abc488f8fa32285bdbeb05cfaf4a
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-10-18 22:37:39 -0400

    remove unused command menu styles

--- components/toolbar/command-menu.tsx
+++ components/toolbar/command-menu.tsx
@@ -262,7 +262,6 @@ export function CommandMenu({
       <Command
         value={selectedValue}
         onValueChange={setSelectedValue}
-        className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
       >
         <CommandInput
           placeholder={
