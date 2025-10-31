06a9a9c145a35f4fc6d10244d31e5423735a10bc
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-10-31 15:39:44 -0400

    fix: add mac keybinding to command menu to use control

--- wxt.config.ts
+++ wxt.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       "open-command-menu": {
         suggested_key: {
           default: "Ctrl+Space",
+          mac: "MacCtrl+Space",
         },
         description: "Open Tabby command menu",
       },
